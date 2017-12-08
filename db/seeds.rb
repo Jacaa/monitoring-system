199e8b62054692afda5414fd778f99faacd89943
Jacek <mikolajczak.jaca@gmail.com>
2017-12-08 01:27:08 +0100

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,46 @@
 Event.destroy_all
 
-10.times do |n|
+a = false
+13.times do |n|
+  a = !a
   Event.create!(
-    walked_in: true,
-    photo: 'link',
-    created_at: (48+n).hours.ago
+    walked_in: a,
+    photo: 'no photo',
+    created_at: 14.days.ago
   )
 end
 
-10.times do |n|
+9.times do |n|
+  Event.create!(
+    walked_in: true,
+    photo: 'no photo',
+    created_at: 13.days.ago
+  )
+end
+
+7.times do |n|
   Event.create!(
     walked_in: false,
     photo: 'no photo',
-    created_at: (24+n).hours.ago
+    created_at: 12.days.ago
+  )
+end
+
+3.times do |n|
+  Event.create!(
+    walked_in: false,
+    photo: 'no photo',
+    created_at: 8.days.ago
   )
 end
 
 a = true
-15.times do |n|
+11.times do |n|
   a = !a
   Event.create!(
     walked_in: a,
-    photo: 'link',
-    created_at: (n+1).hours.ago
+    photo: 'no photo',
+    created_at: 4.days.ago
   )
 end
 
@@ -31,15 +49,31 @@ a = false
   a = !a
   Event.create!(
     walked_in: a,
-    photo: "link",
-    created_at: (20+n).minutes.ago
+    photo: 'no photo',
+    created_at: (48+n).hours.ago
   )
 end
 
-3.times do |n|
+a = true
+15.times do |n|
+  a = !a
+  Event.create!(
+    walked_in: a,
+    photo: 'no photo',
+    created_at: (24+n).hours.ago
+  )
+end
+
+6.times do |n|
   Event.create!(
     walked_in: true,
     photo: "no photo",
     created_at: (10+n).minutes.ago
     )
 end
+
+Event.create!(
+  walked_in: true,
+  photo: "photo_27112017201657.jpeg"
+  created_at: DateTime.strptime("27/11/2017 20:16:57", "%m/%d/%Y %H:%M:%S")
+)
